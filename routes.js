7c85f57600694049c2aca6a2ac96ea48ece5329a
eclipse21 <ajmalsiddiqui21@gmail.com>
2017-03-22 20:13:54 +0530

    frontend

--- routes.js
+++ routes.js
@@ -8,8 +8,20 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.get('/', (req, res) => {
-  res.writeHead(200, {'content-type':'text/plain'});
-  res.end('Hello world!');
+  /*res.writeHead(200, {'content-type':'text/plain'});
+  res.end('Hello world!');*/
+  /*fs.readFile('client/monitor/monitor.html', (err, data) => {
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.writeHead(200, {'content-type':'text/html'});
+      res.write(data);
+      res.end();
+    }
+  });*/
+  //res.writeHead(200, {'content-type':'text/html'});
+  res.sendFile('client/monitor.html', {root: __dirname});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -7,6 +8,9 @@ const app = express();
 let port = process.env.PORT || 8080;
 
 app.use('/', routes);
+
+app.use(express.static('client'));
+app.use(express.static('client/files'));
 
 app.listen(port, () => {
   console.log('Server running at port ' + port);
