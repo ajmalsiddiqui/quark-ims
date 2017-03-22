1b0959386ce047ace1ae9ce62c91282a09879c97
eclipse21 <ajmalsiddiqui21@gmail.com>
2017-03-23 03:16:07 +0530

    integration phase 1

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const rooms = require('./rooms');
+const mailer = require('./mailer');
 
 const router = express.Router();
 
@@ -30,16 +31,25 @@ router.get('/fire', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.writeHead(200, {'content-type':'application/json'});
   rooms.returnRooms((roomlist) => {
-    /*roomlist.forEach((room) => {
-      res.write(JSON.stringify(room));
-    });*/
     res.write(JSON.stringify(roomlist));
   });
   res.end();
 });
 
 router.post('/fire', (req, res) => {
+  let data = req.body;
+  console.log(data);
+  if(data.estatus){
+    rooms.setE(data.id);
+  }
+});
 
+router.post('/notify', (req, res) => {
+  let data = req.body;
+  mailer.setOptions(req.body.room);
+  mailer.send();
+  res.writeHead(200, {'content-type': 'text/plain'});
+  res.end('Concerned personnel of authority have been notified by email.');
 });
 
 module.exports = router;
